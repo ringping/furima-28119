8ef6e3e3ca91a19de9ab6f129b6d943e7f59278c
ringping <69758575+ringping@users.noreply.github.com>
2020-11-15 16:48:57 +0900

    ログイン・ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+         validates :nickname, presence: true
+
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, } do
+          validates :family_name
+          validates :last_name
+          validates :family_name_kana
+          validates :last_name_kana
+        end
+
+         validates :birthday, presence: true
+
 end

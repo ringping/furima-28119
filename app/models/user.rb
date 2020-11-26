2de5589d3bd35345baa409216aed858cc884489d
ringping <69758575+ringping@users.noreply.github.com>
2020-11-26 12:48:21 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
          validates :nickname, presence: true
+        
+         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,100}+\z/i }
 
-         with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, } do
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
           validates :family_name
           validates :last_name
+         end
+
+         with_options presence:true, format: { with: /\A[ァ-ヶー－]+\z/ } do
           validates :family_name_kana
           validates :last_name_kana
         end

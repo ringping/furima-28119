837be47be7baf1fc9cfb9fb58bab2daf668a73b3
ringping <69758575+ringping@users.noreply.github.com>
2020-11-26 15:33:11 +0900

    rubocopによる修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,21 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i }
 
-         validates :nickname, presence: true
-        
-         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,100}+\z/i }
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    validates :family_name
+    validates :last_name
+  end
 
-         with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
-          validates :family_name
-          validates :last_name
-         end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :family_name_kana
+    validates :last_name_kana
+  end
 
-         with_options presence:true, format: { with: /\A[ァ-ヶー－]+\z/ } do
-          validates :family_name_kana
-          validates :last_name_kana
-        end
-
-         validates :birthday, presence: true
-
+  validates :birthday, presence: true
 end

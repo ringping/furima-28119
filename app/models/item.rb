2f46370137dad7931acfd20593c3b33d601932cf
ringping <69758575+ringping@users.noreply.github.com>
2020-11-28 19:25:03 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,23 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one    :memory
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :day
+  belongs_to :send_address
+  belongs_to :shipping
+  belongs_to :status
+
+  with_options presence: true do
+    validates :name
+    validates :introduce
+    validates :price
+  end
+
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :day_id, numericality: { other_than: 1 } 
+  validates :send_address_id, numericality: { other_than: 1 } 
+  validates :shipping_id, numericality: { other_than: 1 } 
+  validates :status_id, numericality: { other_than: 1 } 
 end

c7216afc4ab88d304148a800524d92970ac5970f
ringping <69758575+ringping@users.noreply.github.com>
2020-11-28 19:24:50 +0900

    active_hashの記述

--- db/migrate/20201108113116_create_items.rb
+++ db/migrate/20201108113116_create_items.rb
@@ -1,8 +1,16 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
-      t.timestamps
+      t.string     :name,            null: false
+      t.text       :introduce,       null: false
+      t.integer    :category_id,     null: false
+      t.integer    :status_id,       null: false
+      t.integer    :shipping_id,     null: false
+      t.integer    :send_address_id, null: false
+      t.integer    :day_id,          null: false
+      t.integer    :price,           null: false
+      t.references :user,            null: false, foreign_key: true
+      t.timestamps                   null: false   
     end
   end
 end

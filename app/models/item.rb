3e9945fd73d6fccbde94961cf436bb73388701db
ringping <69758575+ringping@users.noreply.github.com>
2020-11-29 20:09:36 +0900

    マイグレーションファイルの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,24 +1,26 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one    :memory
   has_one_attached :image
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
+  with_options presence: true do
+    validates :name
+    validates :introduce
+    validates :image
+  end
+
+  validates :price, presence: true, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
+
   belongs_to :category
   belongs_to :day
   belongs_to :send_address
   belongs_to :shipping
   belongs_to :status
 
-  with_options presence: true do
-    validates :name
-    validates :introduce
-    validates :price
-  end
-
-  validates :category_id, numericality: { other_than: 1 } 
-  validates :day_id, numericality: { other_than: 1 } 
-  validates :send_address_id, numericality: { other_than: 1 } 
-  validates :shipping_id, numericality: { other_than: 1 } 
-  validates :status_id, numericality: { other_than: 1 } 
+  validates :category_id, numericality: { other_than: 1 }
+  validates :day_id, numericality: { other_than: 1 }
+  validates :send_address_id, numericality: { other_than: 1 }
+  validates :shipping_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2020_11_26_141359) do
     t.integer "send_address_id", null: false
     t.integer "day_id", null: false
     t.integer "price", null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"

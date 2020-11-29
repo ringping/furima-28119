1a826b3890a3a03c295792591ed4672bdbe5be49
ringping <69758575+ringping@users.noreply.github.com>
2020-11-29 18:02:38 +0900

    javascriptによる販売価格の設定

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,13 @@
 window.addEventListener('load', () => {
-  console.log("OK");
+const priceInput = document.getElementById("item-price");
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  console.log(inputValue);
+const addTaxDom = document.getElementById("add-tax-price");
+const tax = inputValue*0.1;
+addTaxDom.innerHTML = Math.floor(tax);
+const addProfitDom = document.getElementById("profit");
+const profit = (inputValue - tax);
+addProfitDom.innerHTML = Math.floor(profit);
+})
 });

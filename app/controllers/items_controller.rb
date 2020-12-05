14fbc4def1ceca61823211b4e1aaa8fbca72adc3
ringping <69758575+ringping@users.noreply.github.com>
2020-12-05 19:49:04 +0900

    show、edit,destoryアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_item, only: [:show, :edit,]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -19,10 +20,23 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def destriy
+    item = Item.find(params[:id]).destroy
+  end
   private
 
   def item_params
     params.require(:item).permit(:name, :introduce, :category_id, :status_id,
                                  :shipping_id, :send_address_id, :day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end

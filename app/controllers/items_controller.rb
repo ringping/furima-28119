ccd2ec380031b8626da59c0c55ca92e656bd673b
ringping <69758575+ringping@users.noreply.github.com>
2020-12-06 22:33:34 +0900

    destroyアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_index, only: [:edit]
 
@@ -34,9 +34,12 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
-  # def destriy
-  # item = Item.find(params[:id]).destroy
-  # end
+
+  def destroy
+   @item = Item.find(params[:id])
+   @item.destroy
+   redirect_to root_path
+  end
 
   private
 

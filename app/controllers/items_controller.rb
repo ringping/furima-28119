62f75c7cbe99624156ef0a36fd1a3318cb63590e
ringping <69758575+ringping@users.noreply.github.com>
2020-12-06 16:22:40 +0900

    updateアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
-  before_action :set_item, only: [:show, :edit,]
+  before_action :set_item, only: [:show, :edit]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -26,6 +26,14 @@ class ItemsController < ApplicationController
   def edit
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+       redirect_to item_path
+    else
+      render :edit
+    end
+  end
   #def destriy
     #item = Item.find(params[:id]).destroy
   #end

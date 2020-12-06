8a408b009752d0e0769e4e5bc493ab96b05e2f20
ringping <69758575+ringping@users.noreply.github.com>
2020-12-06 20:02:21 +0900

    move_to_indexの適用範囲の変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.all.order('created_at DESC')

8073c7d8e3e4370133eab9c668440d11f3ddbddb
ringping <69758575+ringping@users.noreply.github.com>
2020-12-07 19:31:51 +0900

    重複する記述をまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-  before_action :set_item, only: [:show, :edit, :update]
-  before_action :move_to_index, only: [:edit]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit,]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -36,7 +36,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-   @item = Item.find(params[:id])
    @item.destroy
    redirect_to root_path
   end

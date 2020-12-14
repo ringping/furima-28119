444150419e02d7a9a350d2f0c8fa8fe28c343f5a
ringping <69758575+ringping@users.noreply.github.com>
2020-12-14 21:51:42 +0900

    formオブジェクトの導入

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,12 +1,13 @@
 class UserOrder
   include ActiveModel::Model
-  attr_accessor :post_number, :prefecture_id, :city, :city_number, :phone_number, :building, :order
+  attr_accessor :post_number, :prefecture_id, :city, :city_number, :phone_number, :building, :order_id
   with_options presence: true do
-    validates :post_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :post_number,format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :city_number  
-    validates :phone_number, format: {with: /\A\d{11}\z/, message: "is invalid."}
-    validates :order
+    validates :phone_number,format: {with: /\A\d{11}\z/, message: "is invalid."}
+    validates :building
+    validates :order_id
   end
 
     with_options numericality: { other_than: 1 } do
@@ -14,7 +15,7 @@ class UserOrder
     end
 
   def save
-    Address.create(post_number: post_number, prefecture_id: prefecture_id, city: city, city_number: city_number, building: building,phone_number: phone_number)
-    Order.create(item_id: item_id, user_id: user_id)
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create(post_number: post_number, prefecture_id: prefecture_id, city: city, city_number: city_number, phone_number: phone_number, building: building, order_id: order.id)
   end
 end

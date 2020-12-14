5581f848d9c50ca00893eb9fc62ad26096025313
ringping <69758575+ringping@users.noreply.github.com>
2020-12-14 21:48:18 +0900

    マイグレーションファルの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :move_to_root, only:[:index, :create]
  def index
   @item = Item.find(params[:item_id])
   @user_order = UserOrder.new
@@ -6,6 +8,7 @@ class OrdersController < ApplicationController
 
  def create
   binding.pry
+  @item = Item.find(params[:item_id])
   @user_order = UserOrder.new(user_order_params)
   if @user_order.valid?
     @user_order.save
@@ -18,7 +21,11 @@ class OrdersController < ApplicationController
 
  private
   def user_order_params
-    params.require(:user_order).permit(:post_number, :city, :city_number, :phone_number, :order, :prefecture_id, :item_id, :user_id)
+    params.require(:user_order).permit(:post_number, :city, :city_number, :phone_number, :order, :prefecture_id,).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def move_to_root
+    redirect_to root_path if current_user.id == @item.user_id || @item = nil
   end
 
 end

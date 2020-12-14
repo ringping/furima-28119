b3021d15a9d229dea054c4b4382180266a52c602
ringping <69758575+ringping@users.noreply.github.com>
2020-12-14 21:53:04 +0900

    usr_orderに関する単体テストコード

---
+++ spec/factories/user_orders.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :user_order do
+      post_number             {"000-3232"}
+      prefecture_id           {"2"}
+      city                    {Faker::Address.city}    
+      city_number             {Faker::Address.zip}
+      building                {Faker::Address.street_name}
+      phone_number            {"09012345678"}
+      order_id                {"3"}
+  end
+end
